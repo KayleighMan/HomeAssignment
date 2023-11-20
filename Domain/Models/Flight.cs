025a269f7d4d9c95d2ff718682fccc9c8c0771af
kayma <kayma@DESKTOP-U0JL6DK>
2023-11-20 18:11:32 +0100

    Created the classes

---
+++ Domain/Models/Flight.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Flight
+    {
+        public Flight()
+        {
+            Id = Guid.NewGuid();
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public string CountryFrom { get; set; }
+        public string CountryTo { get; set; }
+        public double WholesalePrice { get; set; }
+        public bool CommissionRate { get; set; }
+    }
+}
---
+++ Domain/Models/Ticket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Ticket
+    {
+        public Ticket() 
+        {
+            Id = Guid.NewGuid();
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        [ForeignKey(nameof(Flight))]
+        public Guid FlightIdFK { get; set; }
+        public virtual Flight Flight { get; set; }
+        public string Passport { get; set; }
+        public double PricePaid { get; set; }
+
+        public bool Cancelled { get; set; }
+
+
+
+
+    }
+}
